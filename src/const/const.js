cf3dcd11838ff7b3027de7d806ce6bf52a43360f
Rodriguez710 <dr7557488@gmail.com>
2025-03-20 18:25:49 +0100

    Version inicial

---
+++ src/const/const.js
@@ -0,0 +1,193 @@
+export const studies = [
+  {
+    title: "Grado Superior Desarrollo de Aplicaciones Web",
+    corporation: "IES Los Albares (Cieza-Murcia) ",
+    date: "09/2024 – Actualidad",
+  },  
+  {
+    title: "Grado Superior Desarrollo de Aplicaciones Multiplataforma",
+    corporation: "IES Vicente Medina (Archena-Murcia) ",
+    date: "09/2022 – 07/2024",
+  },
+  {
+    title: "Grado Medio Sistemas Microinformáticos y Redes",
+    corporation: "IES Francisco de Goya (Molina de Segura-Murcia)",
+    date: "09/2020 – 07/2022",
+  },
+  {
+    title: "Educación Secundaria Obligatoria",
+    corporation: "Colegio El Ope (Archena-Murcia)",
+    date: "09/2026 – 07/2020",
+  },
+];
+
+export const IconKeys = {
+  html: "html",
+  css: "css",
+  javaScript: "javaScript",
+  astro: "astro",
+  typeScript: "typeScript",
+  git: "git",
+  github: "github",
+  nodejs: "nodejs",
+  python: "python",
+  angular: "angular",
+  vmware: "vmware",
+  qlik: "qlik",
+  php: "php",
+  linux: "linux",
+  windows: "windows",
+  lua: "lua",
+  symfony: "symfony",
+  msql: "msql",
+  mysql: "mysql",
+  c: "c",
+  excel: "excel",
+  angular: "angular"
+  // here add more icon keys
+};
+
+export const skills = [
+  {
+    title: "Html",
+    icon: IconKeys.html,
+  },
+  {
+    title: "Css",
+    icon: IconKeys.css,
+  },
+  {
+    title: "JavaScript",
+    icon: IconKeys.javaScript,
+  },
+  {
+    title: "Astro",
+    icon: IconKeys.astro,
+  },
+  {
+    title: "Git",
+    icon: IconKeys.git,
+  },
+
+  {
+    title: "Github",
+    icon: IconKeys.github,
+  },
+  {
+    title: "Node Js",
+    icon: IconKeys.nodejs,
+  },
+  {
+    title: "TypeScript",
+    icon: IconKeys.typeScript,
+  },
+  {
+    title: "Python",
+    icon: IconKeys.python,
+  },
+  {
+    title: "Angular",
+    icon: IconKeys.angular,
+  },
+  {
+    title: "Php",
+    icon: IconKeys.php,
+  },
+  {
+    title: "Qlik",
+    icon: IconKeys.qlik,
+  },
+  {
+    title: "MySQL",
+    icon: IconKeys.mysql,
+  },
+  {
+    title: "Microsoft SQL Server",
+    icon: IconKeys.msql,
+  },
+  {
+    title: "Lua",
+    icon: IconKeys.lua,
+  },
+  {
+    title: "Symfony",
+    icon: IconKeys.symfony,
+  },
+  {
+    title: "Vmware",
+    icon: IconKeys.vmware,
+  },
+  {
+    title: "Windows",
+    icon: IconKeys.windows,
+  },
+  {
+    title: "Linux",
+    icon: IconKeys.linux,
+  },
+  {
+    title: "C#",
+    icon: IconKeys.c,
+  },
+  {
+    title: "Excel",
+    icon: IconKeys.excel
+  }
+];
+
+export const projects = [
+  {
+    img: "../../../img/comedorconecta.webp",
+    title: "Comedor Conecta",
+    link: "https://github.com/Rodriguez710/Comedor-Conecta",
+    description: `Proyecto FullStack de una aplicación para comedores escolares donde podemos añadir alumnos y madres por curso o importar directamente desde un fichero CSV.  `,
+    technologies: [
+      {
+        icon: IconKeys.python,
+      },
+      {
+        icon: IconKeys.mysql,
+      },
+    ],
+  },
+  {
+    img: "../../../img/dts.webp",
+    title: "Compras Delegadas DTS",
+    link: "https://github.com/Rodriguez710/Compras-delegadas-DTS-PRO-",
+    description: `Desarrollo de una DTS en Microsoft SQL Server con C# para procesar datos de Excel y filtrarlos automáticamente en una tabla SQL.`,
+    technologies: [
+      {
+        icon: IconKeys.msql,
+      },
+      {
+        icon: IconKeys.excel,
+      },
+      {
+        icon: IconKeys.c,
+      },
+      {
+        icon: IconKeys.windows,
+      }
+    ],
+  },
+  {
+    img: "../../../img/instintorp.webp",
+    title: "InstintoRP",
+    link: "https://github.com/Rodriguez710/InstintoRP",
+    description: `Servidor de GTA 5 en Fivem alojado en ZapHosting. Implementación de scripts innovadores y optimización del entorno de rol.`,
+    technologies: [
+      {
+        icon: IconKeys.html,
+      },
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.javaScript,
+      },
+      {
+        icon: IconKeys.lua,
+      },
+    ],
+  },
+    ]
