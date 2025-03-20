7549042c4ce24bff26728af51ee8c84f9b5992c6
Rodriguez710 <dr7557488@gmail.com>
2025-03-20 19:11:39 +0100

    Segundo commit

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/generic_CPj5UKLt.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/generic_FobV0g1i.mjs').then(n => n.g);
+
+export { page };
---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index_DUdizwRD.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/index_B9ue8gbR.mjs');
+
+export { page };
